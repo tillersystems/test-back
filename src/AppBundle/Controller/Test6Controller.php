1c536cdaa34db381fd19d409acbca5f01ac2f171
vincentoliveira <vincent@oliveira.fr>
2016-09-06 15:27:00 +0200

    test6 done

--- src/AppBundle/Controller/Test6Controller.php
+++ src/AppBundle/Controller/Test6Controller.php
@@ -10,24 +10,67 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class Test6Controller extends Controller
 {
     /**
+     * Result
+     * 
+     * @var array 
+     */
+    private $results = array();
+
+    /**
      * @Route("/test6", name="test6")
      * @Method({"POST"})
      */
     public function test6Action()
     {
-        /**
-         * @TODO: 
-         *  
-         * Write a program that outputs all possibilities to put + or - or 
-         * nothing between the numbers 1, 2, ..., 9 (in this order) such that 
-         * the result is always 100. 
-         * 
-         * For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
-         * Formatted as "1+2+34-5+67-8+9".
-         */
-        
-        $results = null;
-        
-        return new JsonResponse($results, JsonResponse::HTTP_OK);
+        $this->compute();
+
+        return new JsonResponse($this->results, JsonResponse::HTTP_OK);
     }
+    
+    private function compute($index = 1, $tokens = array(0))
+    {
+        if ($index > 9) {
+            $this->handle($tokens);
+            return;
+        }
+        
+        // sum
+        $this->compute($index + 1, array_merge($tokens, ['+', $index]));
+        // diff
+        $this->compute($index + 1, array_merge($tokens, ['-', $index]));
+        // nothing
+        $lastPos = count($tokens) - 1;
+        $tokens[$lastPos] = $tokens[$lastPos] * 10 + $index;
+        $this->compute($index + 1, $tokens);
+    }
+    
+    private function handle($tokens)
+    {
+        $length = count($tokens);
+        
+        $string = '';
+        $result = 0;
+        for ($i = 0; $i < $length; ++$i) {
+            $token = $tokens[$i];
+            if ($token === '+') {
+                $operand = $tokens[++$i];
+                $result += $operand;
+                $string .= '+' . $operand;
+            } elseif ($token === '-') {
+                $operand = $tokens[++$i];
+                $result -= $operand;
+                $string .= '-' . $operand;
+            } elseif ($token === 0) {
+                ++$i;
+            } else {
+                $result = $tokens[$i];
+                $string = $tokens[$i];
+            }
+        }
+        
+        if ($result === 100) {
+            $this->results[] = $string;
+        }
+    }
+
 }
