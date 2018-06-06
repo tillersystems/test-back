2c1d5e258e801c37c72f44a716debe2d5effaefa
Jeremy Foucault <jfoucault@tillersystems.com>
2018-06-06 16:04:48 +0200

    [lookatme] lel

--- src/AppBundle/Controller/Test4Controller.php
+++ src/AppBundle/Controller/Test4Controller.php
@@ -28,13 +28,17 @@ class Test4Controller extends Controller
         }
         
         $result = [];
-        $first20 = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368];
-        for ($i = 0; $i < $n && $i < 25; ++$i) {
-            $result[] = $first20[$i];
-        }
-        
-        for ($i = 25; $i < $n; ++$i) {
-            $result[] = $result[$i - 1] + $result[$i - 2];
+        for ($i = 0; $i < $n; ++$i) {
+            if (array_key_exists($i-1, $result) && array_key_exists($i-2, $result)) {
+                $result[] = $result[$i-1] + $result[$i-2];
+            }
+            else {
+                $result[] = $i;
+            }
+            /**
+            * Another easier way to do this :
+            * $result[] = round(pow((sqrt(5)+1)/2, $i) / sqrt(5));
+            */
         }
         
         return new JsonResponse($result, JsonResponse::HTTP_OK);
